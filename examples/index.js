89f29f2941193d0ee5049387ff462f4aad08cc93
Jarrett Hawrylak <jjhawrylak+git@gmail.com>
2013-08-02 00:02:46 -0400

    more

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 var mktorrent = require('../lib/mktorrent'),
 	util = require('util');
-var t = new mktorrent.Torrent;
+var t = mktorrent.create('sampledata.txt');
 t.save(__dirname + '/test.torrent');
 mktorrent.read(__dirname + '/test.torrent',function(data) {
 	console.log(util.inspect(data));
